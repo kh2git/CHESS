d373eada45d48e9e1b6027d865f13a9f42ab39ed
puckqook <51189338+puckqook@users.noreply.github.com>
2019-11-28 00:53:55 +0900

    Update chess.c

--- CHESSGAME/chess.c
+++ CHESSGAME/chess.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <termio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void print_chess();
 void titleDraw();
 void menuDraw();
 void howtouse();
 int getch();
+char chessboard[8][8][2]= { 
+                            {"R","N","B","Q","K","B","N","R"},
+                            {"P","P","P","P","P","P","P","P"},
+                            {".",".",".",".",".",".",".","."},
+                            {".",".",".",".",".",".",".","."},
+                            {".",".",".",".",".",".",".","."},
+                            {".",".",".",".",".",".",".","."},
+                            {"p","p","p","p","p","p","p","p"},
+                            {"r","n","b","q","k","b","n","r"}
+                            };
+const char ind[8][8][3]=
+                        {
+                        {"A1","A2","A3","A4","A5","A6","A7","A8"},
+                        {"B1","B2","B3","B4","B5","B6","B7","B8"},
+                        {"C1","C2","C3","C4","C5","C6","C7","C8"},
+                        {"D1","D2","D3","D4","D5","D6","D7","D8"},
+                        {"E1","E2","E3","E4","E5","E6","E7","E8"},
+                        {"F1","F2","F3","F4","F5","F6","F7","F8"},
+                        {"G1","G2","G3","G4","G5","G6","G7","G8"},
+                        {"H1","H2","H3","H4","H5","H6","H7","H8"},
+                        };
+char* white_loc = "W1";  // 단순 배열 초기화
+char* black_loc = "B1";  // 단순 배열 초기화
+char *w_now, *b_now, *w_input, *b_input;  // 현재 흑백말 위치와 이동하고 싶은 말의 위치
 
 int main(){
    system("clear");
@@ -14,6 +40,9 @@ int main(){
    scanf("%d",&keyCode);
    switch(keyCode){
       case 1:
+         while(1){
+          print_chess();  
+         }
          break;
       case 2:
          howtouse();
@@ -22,7 +51,7 @@ int main(){
          exit(0);
          break;
    }
-      
+   
    return 0;
 }
 
@@ -84,4 +113,29 @@ int getch(void)
 
 
         return ch;
+}
+
+void input_loc_w() {
+    printf("흰색 입력 : ");
+    scanf("%s %s", &w_now, &w_input);
+    white_loc = &w_input;
+}
+
+void input_loc_b(){
+    printf("검은색 입력 : ");
+    scanf("%s %s", &b_now, &b_input);
+    black_loc = &b_input;
+}
+
+void print_chess() {
+	int i;
+    printf("\t      A     B     C     D     E     F     G     H\n");
+    for ( i = 0; i < 8; i++) {
+        printf("\t   #################################################\n");
+        printf("\t   #     #     #     #     #     #     #     #     #\n");
+        printf("\t %d #  %s  #  %s  #  %s  #  %s  #  %s  #  %s  #  %s  #  %s  #\n", 8 - i, chessboard[i][0], chessboard[i][1], chessboard[i][2],
+            chessboard[i][3], chessboard[i][4], chessboard[i][5], chessboard[i][6], chessboard[i][7]);
+        printf("\t   #     #     #     #     #     #     #     #     #\n");
+    }
+    printf("\t   #################################################\n");
 }
